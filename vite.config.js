cd9964408f04db5c245a21708c6356bea5099619
MartinsRoberto <robertomartinsramm@gmail.com>
2023-02-27 21:55:45 -0300

    github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/2023-calc_imc',
+  base: '/react_calc_imc',
   plugins: [react()],
 })
